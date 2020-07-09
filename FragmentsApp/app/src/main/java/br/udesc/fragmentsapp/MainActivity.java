05964a4ffc35dbb9ef20bee38d0a7b58885937bf
adamiv <valmor_adami_jr@whirlpool.com>
2020-07-09 18:53:44 -0300

    adding the fragmentApp.

---
+++ FragmentsApp/app/src/main/java/br/udesc/fragmentsapp/MainActivity.java
@@ -0,0 +1,43 @@
+package br.udesc.fragmentsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.SeekBar;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //
+        SeekBar seekBar = findViewById(R.id.seek1);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                Log.d(TAG, "onProgressChanged: " + i + " bool: " + b);
+                
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        //
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.add(R.id.frame1, new TextFragment());
+        ft.commit();
+
+    }
+}
