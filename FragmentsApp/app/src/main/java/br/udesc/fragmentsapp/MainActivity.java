100d28614bf67daf9428f022ede3a71d9242b4dd
adamiv <valmor_adami_jr@whirlpool.com>
2020-07-13 19:19:37 -0300

    Atualizando com os eventos de Seekbar.

--- FragmentsApp/app/src/main/java/br/udesc/fragmentsapp/MainActivity.java
+++ FragmentsApp/app/src/main/java/br/udesc/fragmentsapp/MainActivity.java
@@ -1,6 +1,7 @@
 package br.udesc.fragmentsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
@@ -15,12 +16,35 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.add(R.id.frame1, new TextFragment());
+        ft.commit();
+        //
         SeekBar seekBar = findViewById(R.id.seek1);
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
-            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
-                Log.d(TAG, "onProgressChanged: " + i + " bool: " + b);
-                
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean b) {
+                Log.d(TAG, "onProgressChanged: " + progress + " bool: " + b);
+                FragmentManager fm = getSupportFragmentManager();
+                FragmentTransaction ft = fm.beginTransaction();
+                Fragment selectedFragment = new TextFragment();
+                //
+                switch(progress) {
+//                    default:
+//                    case 0:
+//                        //
+//                        break;
+                    case 1:
+                        selectedFragment = new Fragment2();
+                        break;
+                    case 2:
+                        selectedFragment = new Fragment3();
+                        break;
+                }
+                //
+                ft.replace(R.id.frame1, selectedFragment);
+                ft.commit();
             }
 
             @Override
@@ -33,11 +57,7 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-        //
-        FragmentManager fm = getSupportFragmentManager();
-        FragmentTransaction ft = fm.beginTransaction();
-        ft.add(R.id.frame1, new TextFragment());
-        ft.commit();
+
 
     }
 }
